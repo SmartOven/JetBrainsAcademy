9d8f93113177ca1d8df070659eb8920ea5ec76eb
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-07-10 13:59:07 +0300

    Created Cinema Room REST Service project (module)

---
+++ CinemaRoomRESTService/src/main/java/com/github/smartoven/cinemaroomrestservice/CinemaRoomRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.smartoven.cinemaroomrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRoomRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRoomRestServiceApplication.class, args);
+    }
+
+}
