8affb195a42bc86e1f23a926faa35987db5db309
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-07-19 15:22:05 +0300

    Renamed ExceptionHandler to RequestExceptionHandler

--- CinemaRoomRESTService/src/main/java/cinema/exceptions/RequestExceptionHandler.java
+++ CinemaRoomRESTService/src/main/java/cinema/exceptions/RequestExceptionHandler.java
@@ -4,17 +4,18 @@ import cinema.pojo.info.CustomRequestExceptionInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(CustomRequestException.class)
+public class RequestExceptionHandler {
+    @ExceptionHandler(CustomRequestException.class)
     public final ResponseEntity<CustomRequestExceptionInfo> handleTicketAlreadyPurchasedException(CustomRequestException e) {
         CustomRequestExceptionInfo error = new CustomRequestExceptionInfo(e.getError());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MissingServletRequestParameterException.class)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<CustomRequestExceptionInfo> handleMissingParams(MissingServletRequestParameterException e) {
         CustomRequestExceptionInfo error = new CustomRequestExceptionInfo("The password is wrong!");
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
