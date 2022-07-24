d8f9679d148ceed305f4f88114eeb76504082669
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-07-25 01:17:31 +0300

    Created MenuFactory

---
+++ CarSharing/src/main/java/carsharing/util/menu/factory/MenuFactory.java
@@ -0,0 +1,44 @@
+package carsharing.util.menu.factory;
+
+import carsharing.util.menu.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuFactory {
+    private static final Map<String, Menu> menus;
+
+    static {
+        menus = new HashMap<>();
+    }
+
+    public static MainMenu getMainMenu() {
+        MainMenu mainMenu = (MainMenu) menus.getOrDefault("mainMenu", new MainMenu());
+        menus.putIfAbsent("mainMenu", mainMenu);
+        return mainMenu;
+    }
+
+    public static ManagerMenu getManagerMenu() {
+        ManagerMenu managerMenu = (ManagerMenu) menus.getOrDefault("managerMenu", new ManagerMenu());
+        menus.putIfAbsent("managerMenu", managerMenu);
+        return managerMenu;
+    }
+
+    public static CreateCompanyMenu getCreateCompanyMenu() {
+        CreateCompanyMenu createCompanyMenu = (CreateCompanyMenu) menus.getOrDefault("createCompanyMenu", new CreateCompanyMenu());
+        menus.putIfAbsent("createCompanyMenu", createCompanyMenu);
+        return createCompanyMenu;
+    }
+
+    public static CompanyChoosingMenu getCompanyChoosingMenu() {
+        CompanyChoosingMenu companyChoosingMenu = (CompanyChoosingMenu) menus.getOrDefault("companyChoosingMenu", new CompanyChoosingMenu());
+        menus.putIfAbsent("companyChoosingMenu", companyChoosingMenu);
+        return companyChoosingMenu;
+    }
+
+    public static CompanyActionsMenu getCompanyActionsMenu() {
+        CompanyActionsMenu companyActionsMenu = (CompanyActionsMenu) menus.getOrDefault("companyActionsMenu", new CompanyActionsMenu());
+        menus.putIfAbsent("companyActionsMenu", companyActionsMenu);
+        return companyActionsMenu;
+    }
+}
